b58227d0ffaa06eb7880ba7cd16561111deda79d
Kartikay Jainwal <kartikayjainwal@gmail.com>
2026-03-13 22:12:45 +0000

    chore: bump version to v0.3.4

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "notesmd-cli",
 	Short:   "Interact with Obsidian vaults from the terminal",
-	Version: "v0.3.3",
+	Version: "v0.3.4",
 	Long:    "Interact with Obsidian vaults from the terminal",
 }
 
